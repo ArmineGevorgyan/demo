df28c4db17bd9bf65908933b06c1c8ff1c0f31fd
ao-armine <armine@aobyte.com>
2020-09-07 14:21:24 +0400

    Added i18n localization

--- App.js
+++ App.js
@@ -3,14 +3,18 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import store from "./src/redux/store";
+import i18n from "./src/i18n";
+import { I18nextProvider } from "react-i18next";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Welcome to Draper Rhino!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <I18nextProvider i18n={i18n}>
+        <View style={styles.container}>
+          <Text>Welcome to Draper Rhino!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </I18nextProvider>
     </Provider>
   );
 }
