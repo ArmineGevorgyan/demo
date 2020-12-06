bb87c34814f2b61116946124b92354577d8664d8
Harut11132 <harut@aobyte.com>
2020-12-06 14:51:16 +0400

    DAPP-222-notification-item

--- src/redux/ducks/notifications.js
+++ src/redux/ducks/notifications.js
@@ -1,10 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import { API_URL } from "../../config";
+import store from "../store";
 
 const initialState = {
   isLoading: false,
+  loadingMore: false,
   notifications: null,
+  noMoreNotifications: false,
+  page: 0,
   error: null,
 };
 
@@ -18,12 +22,31 @@ const notificationsSlice = createSlice({
     }),
     getNotificationsSuccess: (state, action) => ({
       ...state,
-      isLoading:false,
+      isLoading: false,
       notifications: action.payload,
     }),
     getNotificationsFail: (state, action) => ({
       ...state,
-      isLoading:false,
+      isLoading: false,
+      error: action.payload,
+    }),
+    loadMoreNotifications: (state) => ({
+      ...state,
+      loadingMore: true,
+    }),
+    noMoreNotifications: (state) => ({
+      ...state,
+      noMoreNotifications: true,
+      loadingMore: false,
+    }),
+    loadMoreNotificationsSuccess: (state, action) => ({
+      ...state,
+      page: state.page + 1,
+      notifications: [...state.notifications, ...action.payload]
+    }),
+    loadMoreNotificationsFail: (state, action) => ({
+      ...state,
+      loadingMore:false,
       error: action.payload,
     }),
   }
@@ -35,8 +58,38 @@ export const getNotifications = () => {
   return (dispatch) => {
     dispatch(notificationsSlice.actions.getNotifications());
 
-    //TODO implement get notifications request
+    axios
+      .get(`${API_URL}/notification-messages/current?page=0&size=5`)
+      .then((r) => {
+        return r.data;
+      })
+      .then((data) => {
+        dispatch(notificationsSlice.actions.getNotificationsSuccess(data));
+      })
+      .catch((error) => {
+        dispatch(notificationsSlice.actions.getNotificationsFail(error));
+      });
   };
 };
+
+export const loadMoreNotifications = () => {
+  const state = store.getState();
+  const notificationsState = state.notifications;
+  return (dispatch) => {
+    dispatch(notificationsSlice.actions.loadMoreNotifications());
+
+    axios
+      .get(`${API_URL}/notification-messages/current?page=${notificationsState.page}&size=10`)
+      .then((r) => {
+        return r.data;
+      })
+      .then((data) => {
+        dispatch(notificationsSlice.actions.loadMoreNotificationsSuccess(data));
+      })
+      .catch((error) => {
+        dispatch(notificationsSlice.actions.loadMoreNotificationsFail(error));
+      });
+  }
+}
 
 export default notificationsReduser;
