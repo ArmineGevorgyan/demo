a56eec488227ee7678a2a44534d7228e8ed02111
ao-armine <armine@aobyte.com>
2020-12-07 11:30:47 +0400

    DAPP-228 Register for push notifications

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
+import * as Notifications from "expo-notifications";
+import * as Permissions from "expo-permissions";
+import Constants from "expo-constants";
 import React from "react";
+import { YellowBox, Platform } from "react-native";
 import { Root } from "native-base";
 import { Provider } from "react-redux";
 import { I18nextProvider } from "react-i18next";
+import { useFonts } from "@use-expo/font";
+import { AppLoading } from "expo";
 import store from "./src/redux/store";
 import i18n from "./src/i18n";
 import AppNavigator from "./AppNavigator";
-import { useFonts } from "@use-expo/font";
-import { AppLoading } from "expo";
-import { YellowBox } from "react-native";
+import { setUserPushToken } from "./src/redux/ducks/user";
 
 YellowBox.ignoreWarnings([""]); // this will disable the yellow warning banners
+
+//how to handle notifications when the app is foregrounded
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
   const [isLoaded] = useFonts({
@@ -36,3 +49,41 @@ export default function App() {
     );
   }
 }
+
+registerForPushNotificationsAsync = async () => {
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Permissions.getAsync(
+      Permissions.NOTIFICATIONS
+    );
+    let finalStatus = existingStatus;
+
+    if (existingStatus !== "granted") {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notifications!");
+      return;
+    }
+
+    const token = (await Notifications.getExpoPushTokenAsync()).data;
+
+    return token;
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+};
+
+registerForPushNotificationsAsync().then((token) =>
+  store.dispatch(setUserPushToken(token))
+);
