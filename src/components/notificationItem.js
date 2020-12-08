4906267d2379bfe1875740f2fa1edd96674d608b
Harut11132 <harut@aobyte.com>
2020-12-08 16:03:15 +0400

    DAPP-222-notification-item

--- src/components/notificationItem.js
+++ src/components/notificationItem.js
@@ -1,12 +1,25 @@
 import { Card, Text, View } from "native-base";
 import React, { Component } from "react";
-import { StyleSheet, Image, } from "react-native";
+import { StyleSheet, Image, TouchableOpacity, } from "react-native";
 import moment from "moment";
 import { compose } from "redux";
+import { connect } from "react-redux";
 import { withTranslation } from "react-i18next";
 import LogoImage from "../../assets/whiteLogo.svg";
+import { setNotificationSeen } from "../redux/ducks/notifications";
+import { date } from "yup";
 
 class NotificationItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSeen: false,
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ isSeen: this.props.data.seen })
+  }
 
   getTime(dateString) {
     const format = "DD/MM/YYYY";
@@ -33,64 +46,86 @@ class NotificationItem extends Component {
     return moment(dateString).format(format);
   };
 
+  setToSeen = () => {
+    this.setState({ isSeen: true });
+  }
+
   render() {
     const { data } = this.props;
 
     return (
-      <Card
-        style={{
-          ...styles.container,
-          backgroundCololr: !data.seen ? "#EFEFEF" : "#FFF"
-        }}
-      >
-        {
-          data.uri ?
-            <Image
-              style={styles.imageContainer}
-              source={{ uri: data.uri }}
-            />
-            :
-            <View style={[
-              styles.imageContainer,
-              styles.logo,
-            ]}>
-              <LogoImage />
-            </View>
-        }
-        <View
+      <TouchableOpacity
+        activeOpacity={1}
+        onPress={() => this.props.setNotificationSeen(data.id, this.setToSeen)}>
+        <Card
           style={{
-            flex: 1,
+            ...styles.container,
+            backgroundCololr: !this.state.isSeen ? "#EFEFEF" : "#FFF"
           }}
         >
-          <View style={styles.textContainer}>
-            <Text
-              style={{
-                fontFamily: data.seen ? "montserrat-regular" : "montserrat-semi-bold"
-              }}
-            >
-              {data.title}
-            </Text>
-            <Text>
-              {this.getTime(data.createdAt)}
-            </Text>
-          </View>
-          <Text
+          {
+            data.data.image ?
+              <Image
+                style={styles.imageContainer}
+                source={{ uri: data.data.image }}
+              />
+              :
+              <View style={[
+                styles.imageContainer,
+                styles.logo,
+              ]}>
+                <LogoImage />
+              </View>
+          }
+          <View
             style={{
-              fontFamily: "montserrat-regular",
-              color: data.seen ? "#707070" : "#1E87F4"
+              flex: 1,
             }}
           >
-            {data.body}
-          </Text>
-        </View>
-      </Card>
+            <View style={styles.textContainer}>
+              <Text
+                style={{
+                  fontFamily: this.state.isSeen ? "montserrat-regular" : "montserrat-semi-bold"
+                }}
+              >
+                {data.title}
+              </Text>
+              <Text>
+                {this.getTime(data.createdAt)}
+              </Text>
+            </View>
+            <Text
+              numberOfLines={3}
+              style={{
+                fontFamily: "montserrat-regular",
+                color: this.state.isSeen ? "#707070" : "#1E87F4"
+              }}
+            >
+              {data.body}
+            </Text>
+          </View>
+        </Card>
+      </TouchableOpacity>
     )
   };
 };
 
 
+const mapStateToProps = (state, props) => {
+
+  return {
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setNotificationSeen: (id, setSeen) => dispatch(setNotificationSeen(id, setSeen)),
+  };
+};
+
 export default compose(
-  withTranslation("translations")
+  withTranslation("translations"),
+  connect(mapStateToProps, mapDispatchToProps)
 )(NotificationItem);
 
 const styles = StyleSheet.create({
