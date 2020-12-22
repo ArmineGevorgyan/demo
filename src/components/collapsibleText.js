7c7747ec5d09d277255bd2393e05965432409da0
Peace4Every1 <paligyan@gmail.com>
2020-12-22 16:44:27 +0400

    Minor fixes

--- src/components/collapsibleText.js
+++ src/components/collapsibleText.js
@@ -7,20 +7,26 @@ import { colors } from "../styles/colors";
 
 const CollapsibleText = ({ textStyle, numberOfLines, t, text }) => {
   const [isCollapsed, setIsCollapsed] = useState(true);
+  const [lineCount, setLineCount] = useState(0);
+
+  const onTextLayout = e => {
+    setLineCount(e.nativeEvent.lines.length);
+  };
 
   return (
     <View>
       <Text
         style={textStyle}
         numberOfLines={isCollapsed ? numberOfLines : null}
+        onTextLayout={onTextLayout}
       >
         {text}
       </Text>
-      <TouchableOpacity onPress={() => setIsCollapsed(!isCollapsed)}>
+      {lineCount > 6 && <TouchableOpacity onPress={() => setIsCollapsed(!isCollapsed)}>
         <Text style={innerStyles.seeText}>
           {isCollapsed ? t("collapsibleText.seeMore") : t("collapsibleText.seeLess")}
         </Text>
-      </TouchableOpacity>
+      </TouchableOpacity>}
     </View>
   )
 }
